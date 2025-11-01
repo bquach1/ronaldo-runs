c2ced409c82b98a9a579fdb99970c51a2a321ddb
Bruce Quach <quachbruce@gmail.com>
2025-11-01 13:58:27 -0500

    initialize base colors for dark mode app

--- ronaldo-runs/app/page.tsx
+++ ronaldo-runs/app/page.tsx
@@ -2,8 +2,8 @@ import NavMenu from "./components/NavMenu";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full flex-col bg-white dark:bg-black">
+    <div className="flex min-h-screen font-sans bg-black dark:bg-black">
+      <main className="flex min-h-screen w-full flex-col bg-black dark:bg-black">
         <NavMenu />
       </main>
     </div>
