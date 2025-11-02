f00e77d0a7704ff1967beff71f0f9e830fb62585
Bruce Quach <quachbruce@gmail.com>
2025-11-02 17:44:52 -0600

    Remove redundant bg-black

--- ronaldo-runs/app/goals/page.tsx
+++ ronaldo-runs/app/goals/page.tsx
@@ -2,8 +2,8 @@ import NavMenu from "../components/NavMenu";
 
 export default function GoalsPage() {
   return (
-    <div className="flex min-h-screen font-sans bg-black dark:bg-black">
-      <main className="flex min-h-screen w-full flex-col bg-black dark:bg-black">
+    <div className="flex min-h-screen font-sans bg-black">
+      <main className="flex min-h-screen w-full flex-col bg-black">
         <NavMenu />
         <div className="text-white text-3xl p-5">Goals Page</div>
       </main>
--- ronaldo-runs/app/page.tsx
+++ ronaldo-runs/app/page.tsx
@@ -2,8 +2,8 @@ import NavMenu from "./components/NavMenu";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen font-sans bg-black dark:bg-black">
-      <main className="flex min-h-screen w-full flex-col bg-black dark:bg-black">
+    <div className="flex min-h-screen font-sans bg-black">
+      <main className="flex min-h-screen w-full flex-col bg-black">
         <NavMenu />
       </main>
     </div>
